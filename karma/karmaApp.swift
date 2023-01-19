dcde31a27b1d163992198c2c9ed743f602efd6ca
Giovanni Demasi <giovanni.demasi@mail.polimi.it>
2023-01-19 20:34:33 +0100

    Creation of emulators settings and initialization for testing

--- karma/karmaApp.swift
+++ karma/karmaApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Firebase
+import FirebaseAuth
 
 @main
 struct karmaApp: App {
@@ -15,7 +16,21 @@ struct karmaApp: App {
     
     
     init() {
+        // Firebase configurtion is always executed
         FirebaseApp.configure()
+        // initialization for testing, using emulators
+        #if EMULATORS
+        print(">> Testing on Emulators <<")
+        Auth.auth().useEmulator(withHost: "localhost", port: 9099)
+        let settings = Firestore.firestore().settings
+        settings.host = "localhost:8080"
+        settings.isPersistenceEnabled = false
+        settings.isSSLEnabled = false
+        Firestore.firestore().settings = settings
+        #elseif DEBUG
+        print(">> Testing on Firebase Server <<")
+        #endif
+        
     }
     
     var body: some Scene {
