383f2296e6b867c51cf16bfb5a9e021666e60dbd
Giovanni Demasi <giovanni.demasi@mail.polimi.it>
2023-01-20 10:00:38 +0100

    Updated app configuration, no errors. TODO: setup must executed only once

--- karma/karmaApp.swift
+++ karma/karmaApp.swift
@@ -7,37 +7,34 @@
 
 import SwiftUI
 import Firebase
+import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
 
 @main
 struct karmaApp: App {
+    //@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var viewModel = AuthViewModel()
     
     
     init() {
-        // Firebase configurtion is always executed
-        FirebaseApp.configure()
-        // initialization for testing, using emulators
-        #if EMULATORS
-        print(">> Testing on Emulators <<")
-        Auth.auth().useEmulator(withHost: "127.0.0.1", port: 9099)
-        //Firestore.firestore().useEmulator(withHost: "127.0.0.1", port: 8080)
-        let settings = Firestore.firestore().settings
-        settings.host = "127.0.0.1:8080"
-        settings.isPersistenceEnabled = false
-        settings.isSSLEnabled = false
-        Firestore.firestore().settings = settings
-        #elseif DEBUG
+        //        // Firebase configurtion is always executed
+        //        FirebaseApp.configure()
+        //        // initialization for testing, using emulators
+        //        #if EMULATORS
+        //        print(">> Testing on Emulators <<")
+        //        Auth.auth().useEmulator(withHost: "127.0.0.1", port: 9099)
+        //        //Firestore.firestore().useEmulator(withHost: "127.0.0.1", port: 8080)
+        //        let settings = Firestore.firestore().settings
+        //        settings.host = "127.0.0.1:8080"
+        //        settings.isPersistenceEnabled = false
+        //        settings.isSSLEnabled = false
+        //        Firestore.firestore().settings = settings
+#if DEBUG
         print(">> Testing on Firebase Server <<")
-        #endif
-
-    }
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication
-                       .LaunchOptionsKey: Any]?) -> Bool {
-      FirebaseApp.configure()
-      return true
+        FirebaseApp.configure()
+#endif
+        
     }
     
     var body: some Scene {
@@ -49,4 +46,12 @@ struct karmaApp: App {
             .environmentObject(viewModel)
         }
     }
+    
 }
+
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//        FirebaseApp.configure()
+//        return true
+//    }
+//}
--- karmaTests/karmaTests.swift
+++ karmaTests/karmaTests.swift
@@ -7,17 +7,30 @@
 
 import XCTest
 @testable import karma
+import Firebase
+import FirebaseFirestore
 
 final class karmaTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
+#if EMULATORS
+        print(">> Testing on Emulators <<")
+#endif
+        FirebaseApp.configure()
+        Auth.auth().useEmulator(withHost: "localhost", port: 9099)
+        //Firestore.firestore().useEmulator(withHost: "127.0.0.1", port: 8080)
+        let settings = Firestore.firestore().settings
+        settings.host = "localhost:8080"
+        settings.isPersistenceEnabled = false
+        settings.isSSLEnabled = false
+        Firestore.firestore().settings = settings
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -25,17 +38,18 @@ final class karmaTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         //self.measure {
-            // Put the code you want to measure the time of here.
+        // Put the code you want to measure the time of here.
         //}
     }
     
     func testRandom () throws{
         let sut = AuthViewModel()
         sut.register(withEmail: "example@gmail.com", password: "example", fullname: "example", username: "example")
+        XCTAssertEqual(sut.didAuthenticateUser, false)
     }
-
+    
 }
