10f797ac38e48c3b62895a0e4bf9fd07f9830972
Giovanni Demasi <giovanni.demasi@mail.polimi.it>
2023-01-20 10:13:17 +0100

    FIX: firebase configuration is executed once

--- karmaTests/karmaTests.swift
+++ karmaTests/karmaTests.swift
@@ -9,21 +9,25 @@ import XCTest
 @testable import karma
 import Firebase
 import FirebaseFirestore
+var iteration : Int = 0
 
 final class karmaTests: XCTestCase {
     
     override func setUpWithError() throws {
 #if EMULATORS
+        
+        iteration += 1
         print(">> Testing on Emulators <<")
 #endif
-        FirebaseApp.configure()
-        Auth.auth().useEmulator(withHost: "localhost", port: 9099)
-        //Firestore.firestore().useEmulator(withHost: "127.0.0.1", port: 8080)
-        let settings = Firestore.firestore().settings
-        settings.host = "localhost:8080"
-        settings.isPersistenceEnabled = false
-        settings.isSSLEnabled = false
-        Firestore.firestore().settings = settings
+        if(iteration == 1){
+            FirebaseApp.configure()
+            Auth.auth().useEmulator(withHost: "localhost", port: 9099)
+            //Firestore.firestore().useEmulator(withHost: "127.0.0.1", port: 8080)
+            let settings = Firestore.firestore().settings
+            settings.host = "localhost:8080"
+            settings.isPersistenceEnabled = false
+            settings.isSSLEnabled = false
+            Firestore.firestore().settings = settings}
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
